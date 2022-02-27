43812c8746b59e1c4c80bd7c43d9452ecb3a457b
iakonyakina <iakonyakina@domclick.ru>
2022-02-28 02:10:40 +0300

    Valid Parentheses

---
+++ src/main/java/com/leetcode/ValidParentheses.java
@@ -0,0 +1,79 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "()"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "()[]{}"
+ * Output: true
+ * Example 3:
+ *
+ * Input: s = "(]"
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ *
+ * https://leetcode.com/problems/valid-parentheses/
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false; // нечетное число символов в строке -> строка невалидна
+
+        Map<Character, Character> brackets = new HashMap<>(3);
+        brackets.put('}', '{');
+        brackets.put(']', '[');
+        brackets.put(')', '(');
+
+        var stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            var c = s.charAt(i);
+            if (!brackets.containsKey(c)) {
+                stack.push(c); // найденный символ - открывающая скобка, сохраняем её в стек
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // найденный символ - закрывающая скобка, при этом стек пуст -> строка невалидна
+                }
+
+                if (stack.pop() != brackets.get(c)) {
+                    return false; // верхний элемент стека - не открывающая скобка, найденный символ - закрывающая скобка -> строка невалидна
+                }
+            }
+        }
+        return stack.isEmpty(); // стек после обработки строки пуст -> строка валидна
+    }
+}
+
+class Main20 {
+    public static void main(String[] args) {
+        var result1 = new ValidParentheses().isValid("()");
+        assert(result1);
+
+        var result2 = new ValidParentheses().isValid("()[]{}");
+        assert(result2);
+
+        var result3 = new ValidParentheses().isValid("(]");
+        assert(!result3);
+
+        var result4 = new ValidParentheses().isValid("({[]})");
+        assert(result4);
+    }
+}
