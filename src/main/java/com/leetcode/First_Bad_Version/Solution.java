88971ee5123b7b7feeb6ccc3ed0fac6d961ac91a
iakonyakina <s-irina-alexandrovna@yandex.ru>
2022-03-07 01:29:44 +0300

    First Bad Version

---
+++ src/main/java/com/leetcode/First_Bad_Version/Solution.java
@@ -0,0 +1,59 @@
+package com.leetcode.First_Bad_Version;
+
+/**
+ * 278. First Bad Version
+ * https://leetcode.com/problems/first-bad-version/
+ *
+ * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ *
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ *
+ * You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ *
+ *
+ *
+ * Example 1:
+ * Input: n = 5, bad = 4
+ * Output: 4
+ * Explanation:
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ * Then 4 is the first bad version.
+ *
+ * Example 2:
+ * Input: n = 1, bad = 1
+ * Output: 1
+ */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        var first = 1;
+        var last = n;
+
+        while (first < last) {
+            var middle = first + (last - first) / 2;
+            if (isBadVersion(middle)) {
+                last = middle;
+            } else {
+                first = middle + 1;
+            }
+        }
+        return first;
+    }
+}
+
+class VersionControl {
+    boolean isBadVersion(int version) {
+        return version >= 1;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        var result = new Solution().firstBadVersion(5);
+//        assert result == 4;
+
+        var result2 = new Solution().firstBadVersion(2);
+        assert result == 1;
+    }
+}
